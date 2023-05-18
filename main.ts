37e3e76a0ea65ab3ed5fc3865512578c65a66514
Ali Al-Shaikh <mi3lix9@outlook.sa>
2023-05-18 03:07:31 +0300

    move BOT_TOKEN to the body of the requset

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 import "https://deno.land/std@0.187.0/dotenv/load.ts";
 import { Application, Router } from "https://deno.land/x/oak/mod.ts";
-import { sendSummaryTo } from "./bot.ts";
+import { bot, sendSummaryTo } from "./bot.ts";
 
 const BOT_TOKEN = Deno.env.get("BOT_TOKEN");
 
@@ -13,11 +13,16 @@ const app = new Application();
 const router = new Router();
 
 router.post("/sendphoto", async (ctx) => {
-  if (ctx.params.BOT_API !== BOT_TOKEN)
-    throw new Error("No BOT_API in the request.");
-  const { text, imageUrl, chatId } = await ctx.request.body({ type: "json" })
-    .value;
+  const {
+    text,
+    imageUrl,
+    chatId,
+    BOT_TOKEN: MyBOT_TOKEEN,
+  } = await ctx.request.body({ type: "json" }).value;
+
+  if (MyBOT_TOKEEN !== BOT_TOKEN) throw new Error("No BOT_API in the request.");
   await sendSummaryTo(chatId, { text, imageUrl });
+  ctx.response.status = 200;
 });
 
 app.use(router.routes());
