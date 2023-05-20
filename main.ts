8c680c258cd1e25f9bb8436732ee3ed8b11c3644
Ali Al-Shaikh <mi3lix9@outlook.sa>
2023-05-20 20:55:01 +0300

    rewrite the code and simlify it.

--- main.ts
+++ main.ts
@@ -1,27 +1,25 @@
 import "https://deno.land/std@0.187.0/dotenv/load.ts";
 import { Application, Router } from "https://deno.land/x/oak/mod.ts";
-import { bot, sendSummaryTo } from "./bot.ts";
-
-const BOT_TOKEN = Deno.env.get("BOT_TOKEN");
-
-if (typeof BOT_TOKEN === "undefined") {
-  throw new Error("BOT_TOKEN is not defined.");
-}
+import { Configuration, OpenAIApi } from "https://esm.sh/openai";
 
 const app = new Application();
 
 const router = new Router();
 
-router.post("/sendphoto", async (ctx) => {
-  const {
-    text,
-    imageUrl,
-    chatId,
-    BOT_TOKEN: MyBOT_TOKEEN,
-  } = await ctx.request.body({ type: "json" }).value;
+router.post("summarize", async (ctx) => {
+  const { text, OPENAI_TOKEN } = await ctx.request.body({ type: "json" }).value;
 
-  if (MyBOT_TOKEEN !== BOT_TOKEN) throw new Error("No BOT_API in the request.");
-  await sendSummaryTo(chatId, { text, imageUrl });
+  const configuration = new Configuration({
+    apiKey: OPENAI_TOKEN,
+  });
+  const openai = new OpenAIApi(configuration);
+
+  const chat = await openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    messages: [{ role: "user", content: text }],
+  });
+
+  ctx.response.body = chat.data.choices[0].message?.content;
   ctx.response.status = 200;
 });
 
